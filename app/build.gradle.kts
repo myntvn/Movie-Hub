ddd341f1583c67acb4f55b245424ab255363ebfa
Thien My <myntuit@gmail.com>
2024-12-13 15:44:20 +0900

    Update target sdk, libraries version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.mynt.app.moviehub"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mynt.app.moviehub"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
