9d48fb4a8dd2b812783c4508c444b5a564488946
Thien My <myntuit@gmail.com>
2024-12-14 13:20:38 +0900

    Implement usecase module

--- usecase/build.gradle.kts
+++ usecase/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -33,6 +35,11 @@ android {
 }
 
 dependencies {
+    implementation(project(":model"))
+    implementation(project(":repository"))
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
     implementation(libs.androidx.core.ktx)
     testImplementation(libs.junit)
